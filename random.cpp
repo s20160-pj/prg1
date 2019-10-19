dc903b455a40a5a521f183202d8abfdcf82dfdb0
PatrykBorucki <s20160@pjwstk.edu.pl>
2019-10-19 17:03:37 +0200

    Zmodyfikowano plik random.cpp. Zastosowano wskaźnik.

--- random.cpp
+++ random.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <random>
 
-void naive_sort(int const N, int an_array[])
+void naive_sort(int const N, int* an_array)
 {
-	for (auto i = 0; i < N; ++i) {
+	for (auto i = 0; i < N; ++i)
+	{
 
-        	auto the_smallest = an_array[i];
-        	auto index = i;
+        	int* the_smallest = &an_array[i];
  
-		for (auto j = i; j < N; ++j) {
-
-		if (an_array[j] < the_smallest) {
-        	        the_smallest = an_array[j];
-			index = j;
+		for (auto j = i; j < N; ++j)
+		{
+			if (an_array[j] < *the_smallest)
+			{
+				the_smallest = &an_array[j];
 			}
 		}
-
-		an_array[index] = an_array[i];
-	
-		an_array[i] = the_smallest;
+		auto tmp = *the_smallest;
+		*the_smallest = an_array[i];
+		an_array[i] = tmp;
 	}
 }
 
@@ -26,8 +25,9 @@ void random_initialisation(int const N, int an_array[])
 {
 	std::random_device rd;
 	std::uniform_int_distribution<int> generator(0, 100);
-	for (auto i = 0; i < N; ++i) {
-	an_array[i] = generator(rd);
+	for (auto i = 0; i < N; ++i)
+	{
+		an_array[i] = generator(rd);
 	}
 }
 
@@ -39,8 +39,9 @@ int main()
 
 	naive_sort(N, an_array);
 
-	for (auto i = 0; i < N; ++i) {
-	std::cout << an_array[i] << "\n";
+	for (auto i = 0; i < N; ++i)
+	{
+		std::cout << an_array[i] << "\n";
 	}
-return 0;
+	return 0;
 }
